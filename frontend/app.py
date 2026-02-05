b7b50bb29b7ae88275484b3887aef16b80d956c2
lasercata <contact@lasercata.com>
2026-02-05 02:18:22 +0100

    frontend: manage admin and user roles

--- frontend/app.py
+++ frontend/app.py
@@ -24,28 +24,48 @@ PLATFORM_URL = 'https://localhost:5000' #TODO: use .env
 #TODO: connect to the DB
 # Simulated user store (replace with database in real-world scenario)
 USERS = {
-    'usr': {
+    'admin': {
+        'uid': '0',
+        'is_admin': True,
+        'password': bcrypt.generate_password_hash('azer').decode('utf-8')
+    },
+    'usr1': {
+        'uid': '39ac70e4',
+        'is_admin': False,
+        'password': bcrypt.generate_password_hash('azer').decode('utf-8')
+    },
+    'usr2': {
+        'uid': '1c25d917',
+        'is_admin': False,
         'password': bcrypt.generate_password_hash('azer').decode('utf-8')
     }
 }
 
 ##-JWT Token functions (authentication)
-def generate_token(username) -> str: #TODO: change param
-    '''Generate a JWT token.'''
+def generate_token(username: str, uid: str, is_admin: bool) -> str:
+    '''Generate a JWT token for the given user that expires after one day.'''
 
-    payload = { #TODO: add fields (role[admin|user], UID, ...)
+    payload = {
+        'uid': uid,
         'username': username,
-        'exp': datetime.now(timezone.utc) + timedelta(hours=1)
+        'is_admin': is_admin,
+        'exp': datetime.now(timezone.utc) + timedelta(days=1) # Token expires after one day
     }
 
     return jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
 
-def verify_token(token):
-    '''Verify and decode JWT token.'''
+def verify_token(token) -> tuple[str, str] | None:
+    '''
+    Verify and decode JWT token.
+
+    Out:
+        None if error
+        (username, is_admin) from the token otherwise
+    '''
 
     try:
         payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        return payload['username'] #TODO: return UID and role?
+        return payload['username'], payload['is_admin']
 
     except jwt.ExpiredSignatureError:
         return None
@@ -62,26 +82,66 @@ def home():
     '''
 
     token = request.cookies.get('token')
-    username = verify_token(token)
+    tk_data = verify_token(token)
 
-    #TODO: different template for admin/user?
-    
-    if not username:
+    if tk_data is None:
         return redirect(url_for('login'))
     
-    return render_template('home.html', username=username)
+    username, is_admin = tk_data
+
+    if is_admin:
+        return render_template('home_admin.html', username=username)
+    else:
+        return render_template('home.html', username=username)
+
+@app.route('/reservation_page')
+def reservation_page():
+    '''
+    Fetch nodes from IoT platform API and load the user reservation page
+    Access restricted to logged users.
+    '''
+
+    token = request.cookies.get('token')
+    if token is None:
+        return jsonify({'error': 'No authentication token'}), 401
+    
+    try:
+        # Make request to IoT platform API
+        response = requests.get(
+            f'{PLATFORM_URL}/nodes?status=free',
+            headers={'Authorization': f'Bearer {token}'}
+        )
+        
+        # Check response from IoT platform
+        if response.status_code == 200:
+            nodes = response.json()
+            return render_template('reservation_page.html', nodes=nodes)
+        else:
+            # Handle authentication or other errors
+            return jsonify({'error': 'Failed to fetch nodes'}), response.status_code
+    
+    except requests.RequestException as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/nodes_page')
 def nodes_page():
     '''
     Fetch nodes from IoT platform API.
-    Access restricted to logged users.
+    Access restricted to admins.
     '''
 
+    # Check that there is a token, and that it is an admin one
     token = request.cookies.get('token')
-    
-    if not token:
+    if token is None:
         return jsonify({'error': 'No authentication token'}), 401
+
+    tk_data = verify_token(token)
+    if tk_data is None:
+        return jsonify({'error': 'No authentication token'}), 401
+    
+    _, is_admin = tk_data
+    if not is_admin:
+        return jsonify({'error': 'Not authorized to access this page'}), 401
     
     try:
         # Make request to IoT platform API
@@ -108,7 +168,18 @@ def users_page():
     Access restricted to admin.
     '''
 
-    token = request.cookies.get('token') #TODO: restrict to admins.
+    # Check that there is a token, and that it is an admin one
+    token = request.cookies.get('token')
+    if token is None:
+        return jsonify({'error': 'No authentication token'}), 401
+
+    tk_data = verify_token(token)
+    if tk_data is None:
+        return jsonify({'error': 'No authentication token'}), 401
+    
+    _, is_admin = tk_data
+    if not is_admin:
+        return jsonify({'error': 'Not authorized to access this page'}), 401
     
     if not token:
         return jsonify({'error': 'No authentication token'}), 401
@@ -141,7 +212,8 @@ def login():
         
         # Check if user exists and password is correct
         if username in USERS and bcrypt.check_password_hash(USERS[username]['password'], password):
-            token = generate_token(username)
+            usr = USERS[username]
+            token = generate_token(username, usr['uid'], usr['is_admin'])
             
             # Create response with token as cookie
             response = make_response(redirect(url_for('home')))
