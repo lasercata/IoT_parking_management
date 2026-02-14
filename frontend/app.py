1986b0bc6a3f1a5044c7bf2a05bfc989f1cd4eb6
lasercata <contact@lasercata.com>
2026-02-14 02:50:27 +0100

    frontend: handling auth in separated file + improvements

--- frontend/app.py
+++ frontend/app.py
@@ -4,12 +4,12 @@
 ##-Imports
 from flask import Flask, render_template, request, jsonify, make_response, redirect, url_for
 from flask_bcrypt import Bcrypt
-import jwt
-from datetime import datetime, timedelta, timezone
 import requests
 import os
 from dotenv import load_dotenv
 from sys import argv
+
+from src.authentication import TokenManager, token_required
 
 ##-Init
 app = Flask(__name__)
@@ -27,10 +27,9 @@ load_dotenv(dotenv_path)
 # Base URL of the IoT platform
 PLATFORM_URL = os.environ.get('PLATFORM_URL', default='http://localhost:5000')
 
-# Secret key for JWT
-# IMPORTANT: use the same secret key as in the backend
-# app.config['SECRET_KEY'] = 'your-shared-secret-key' #TODO: use .env
+# Secret key for JWT (IMPORTANT: use the same secret key as in the backend)
 app.config['SECRET_KEY'] = os.environ.get('JWT_SHARED_TOKEN')
+SECRET_KEY = app.config['SECRET_KEY']
 
 # Retrieve MongoDB connection details from environment variables
 mongo_username = os.environ.get('MONGO_USERNAME')
@@ -59,37 +58,7 @@ USERS = {
     }
 }
 
-##-JWT Token functions (authentication)
-def generate_token(username: str, uid: str, is_admin: bool) -> str:
-    '''Generate a JWT token for the given user that expires after one day.'''
-
-    payload = {
-        'uid': uid,
-        'username': username,
-        'is_admin': is_admin,
-        'exp': datetime.now(timezone.utc) + timedelta(days=1) # Token expires after one day
-    }
-
-    return jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
-
-def verify_token(token) -> tuple[str, str] | None:
-    '''
-    Verify and decode JWT token.
-
-    Out:
-        None if error
-        (username, is_admin) from the token otherwise
-    '''
-
-    try:
-        payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        return payload['username'], payload['is_admin']
-
-    except jwt.ExpiredSignatureError:
-        return None
-
-    except jwt.InvalidTokenError:
-        return None
+token_manager = TokenManager(app.config['SECRET_KEY'])
 
 ##-Routes
 @app.route('/')
@@ -99,31 +68,30 @@ def home():
     Access restricted to logged users.
     '''
 
-    token = request.cookies.get('token')
-    tk_data = verify_token(token)
-
-    if tk_data is None:
+    try:
+        token = token_manager.retrieve_token('cookies')
+    except RuntimeError:
         return redirect(url_for('login'))
-    
-    username, is_admin = tk_data
 
-    if is_admin:
-        return render_template('home_admin.html', username=username)
+    tk_payload = token_manager.decode_token(token)
+
+    if token_manager.is_admin(token):
+        return render_template('home_admin.html', username=tk_payload['username'])
+
     else:
-        return render_template('home.html', username=username)
+        return render_template('home.html', username=tk_payload['username'])
 
 @app.route('/reservation_page')
+@token_required(SECRET_KEY)
 def reservation_page():
     '''
     Fetch nodes from IoT platform API and load the user reservation page
     Access restricted to logged users.
     '''
 
-    token = request.cookies.get('token')
-    if token is None:
-        return jsonify({'error': 'No authentication token'}), 401
-    
     try:
+        token = token_manager.retrieve_token('cookies')
+
         # Make request to IoT platform API
         response = requests.get(
             f'{PLATFORM_URL}/api/nodes?status=free',
@@ -142,26 +110,16 @@ def reservation_page():
         return jsonify({'error': str(e)}), 500
 
 @app.route('/nodes_page')
+@token_required(SECRET_KEY, only_admins=True)
 def nodes_page():
     '''
     Fetch nodes from IoT platform API.
     Access restricted to admins.
     '''
 
-    # Check that there is a token, and that it is an admin one
-    token = request.cookies.get('token') #TODO: move this to a decorator, in an other file (like in platform)
-    if token is None:
-        return jsonify({'error': 'No authentication token'}), 401
-
-    tk_data = verify_token(token)
-    if tk_data is None:
-        return jsonify({'error': 'No authentication token'}), 401
-    
-    _, is_admin = tk_data
-    if not is_admin:
-        return jsonify({'error': 'Not authorized to access this page'}), 401
-    
     try:
+        token = token_manager.retrieve_token('cookies')
+
         # Make request to IoT platform API
         response = requests.get(
             f'{PLATFORM_URL}/api/nodes/',
@@ -180,29 +138,16 @@ def nodes_page():
         return jsonify({'error': str(e)}), 500
 
 @app.route('/users_page')
+@token_required(SECRET_KEY, only_admins=True)
 def users_page():
     '''
     Fetch users from IoT platform API.
     Access restricted to admin.
     '''
-
-    # Check that there is a token, and that it is an admin one
-    token = request.cookies.get('token')
-    if token is None:
-        return jsonify({'error': 'No authentication token'}), 401
-
-    tk_data = verify_token(token)
-    if tk_data is None:
-        return jsonify({'error': 'No authentication token'}), 401
-    
-    _, is_admin = tk_data
-    if not is_admin:
-        return jsonify({'error': 'Not authorized to access this page'}), 401
-    
-    if not token:
-        return jsonify({'error': 'No authentication token'}), 401
     
     try:
+        token = token_manager.retrieve_token('cookies')
+
         # Make request to IoT platform API
         response = requests.get(
             f'{PLATFORM_URL}/api/users',
@@ -231,7 +176,7 @@ def login():
         # Check if user exists and password is correct
         if username in USERS and bcrypt.check_password_hash(USERS[username]['password'], password):
             usr = USERS[username]
-            token = generate_token(username, usr['uid'], usr['is_admin'])
+            token = token_manager.generate_token(username, usr['uid'], usr['is_admin'])
             
             # Create response with token as cookie
             response = make_response(redirect(url_for('home')))
@@ -241,9 +186,16 @@ def login():
         
         return render_template('login.html', error='Invalid credentials')
     
-    return render_template('login.html')
+    else:
+        try:
+            token = token_manager.retrieve_token('cookies') # Check if token is present
+        except RuntimeError:
+            return render_template('login.html') # No token -> go to login
+        else:
+            return redirect(url_for('home')) # token found -> go to home page
 
 @app.route('/pwd_reset', methods=['GET', 'POST'])
+@token_required(SECRET_KEY)
 def pwd_reset(): #TODO
     '''Password reset route handling both GET and POST requests.'''
 
@@ -257,7 +209,7 @@ def pwd_reset(): #TODO
     #     # Check if user exists and password is correct
     #     if username in USERS and bcrypt.check_password_hash(USERS[username]['password'], password):
     #         usr = USERS[username]
-    #         token = generate_token(username, usr['uid'], usr['is_admin'])
+    #         token = token_manager.generate_token(username, usr['uid'], usr['is_admin'])
     #         
     #         # Create response with token as cookie
     #         response = make_response(redirect(url_for('home')))
@@ -268,6 +220,7 @@ def pwd_reset(): #TODO
     #     return render_template('login.html', error='Invalid credentials')
     # 
     # return render_template('login.html')
+    pass
 
 @app.route('/logout')
 def logout():
@@ -277,6 +230,14 @@ def logout():
     response.delete_cookie('token')
     return response
 
+@app.route('/invalid_token')
+def invalid_token():
+    '''Route for invalid token'''
+
+    return render_template('invalid_token.html')
+
+
+##-Run
 if __name__ == '__main__':
     if len(argv) > 1:
         port = int(argv[1])
---
+++ frontend/src/authentication.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''Handles authentication with JWT tokens'''
+
+##-Imports
+from typing import Any
+import jwt
+from datetime import datetime, timedelta, timezone
+from functools import wraps
+from flask import render_template, request, redirect, url_for
+
+##-JWT Token manger
+class TokenManager:
+    '''Manages JWT tokens'''
+
+    def __init__(self, secret_key: str):
+        '''
+        Initiates the class
+
+        In:
+            - secret_key: the JWT secret key (shared with the platform)
+        '''
+
+        self._secret_key = secret_key
+
+
+    def generate_token(self, username: str, uid: str, is_admin: bool) -> str:
+        '''Generates a JWT token for the given user that expires after 24 hours.'''
+
+        payload = {
+            'uid': uid,
+            'username': username,
+            'is_admin': is_admin,
+            'exp': datetime.now(timezone.utc) + timedelta(days=1) # Token expires after one day
+        }
+
+        return jwt.encode(payload, self._secret_key, algorithm='HS256')
+
+    def decode_token(self, token: str) -> dict[str, Any]:
+        '''
+        Tries to decode the token and return its content.
+
+        Out:
+            payload     if successful
+            ValueError  Otherwise
+        '''
+
+        try:
+            # Decode the token
+            payload = jwt.decode(token, self._secret_key, algorithms=['HS256'])
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise ValueError('Token has expired!')
+
+        except jwt.InvalidTokenError:
+            raise ValueError('Invalid token!')
+
+    def is_admin(self, token: str) -> bool:
+        '''
+        Tries to decode the token (using `self.decode_token`) and checks if it is from an admin.
+
+        Out:
+            True       if token is owned by an admin
+            False      if not
+            ValueError if error while decoding
+        '''
+
+        token_payload = self.decode_token(token)
+        return token_payload['is_admin']
+
+    def retrieve_token(self, source: str) -> str:
+        '''
+        Tries to retrieve the token from `source`.
+
+        In:
+            - source: the source of the token. Should be in:
+                + 'cookies': retrieve from cookies;
+                + 'headers': retrieve from headers;
+                + 'first': try cookies, and then headers if not found
+
+        Out:
+            str: the token
+
+        Raises:
+            ValueError   if `source` not in options
+            RuntimeError if token not found
+        '''
+
+        if source not in {'cookies', 'headers', 'first'}:
+            raise ValueError('Argument "source" not correct')
+
+        token = None
+
+        if source in ('cookies', 'first'):
+            token = request.cookies.get('token')
+
+            if token is not None:
+                return token
+
+        if source in ('headers', 'first'):
+            if 'Authorization' in request.headers:
+                return request.headers['Authorization']
+
+        raise RuntimeError('token not found')
+
+##-Decorator
+def token_required(secret_key: str, only_admins: bool = False):
+    '''
+    Creates a decorator that ensure that the connected user possesses a valid token.
+
+    In:
+        - only_admins: if True, restrict access only to admins. Otherwise, restrict access to token bearers.
+    '''
+
+    def decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token_manager = TokenManager(secret_key)
+
+            try:
+                token = token_manager.retrieve_token(source='first')
+                payload = token_manager.decode_token(token)
+
+            except (ValueError, RuntimeError) as err:
+                # return render_template('login.html', error=str(err))
+                return redirect(url_for('invalid_token'))
+
+            # Privilege check
+            if only_admins and not payload['is_admin']:
+                # return render_template('login.html', error='Only admins can access this resource')
+                return redirect(url_for('invalid_token'))
+
+            # If we get here, token is valid
+            return f(*args, **kwargs)
+        
+        return decorated
+
+    return decorator
